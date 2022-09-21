bf9fbddf6abdb46c27037d7fcac90719f137714f
jhonathancs <jhonathan_jcs@hotmail.com>
2022-09-21 09:04:35 -0300

    desafio04

---
+++ exercicios03/desafio004.py
@@ -0,0 +1,8 @@
+a = input("Digite algo: ")
+print('o tipo primitivo desse valor é, ', type(a))
+print('só tem espacos ?, ', a.isspace())
+print('é um numero ?, ', a.isnumeric())
+print('é alfabetico ?, ', a.isalpha())
+print('é maiscula ?, ', a.isupper())
+print('é minuscula ?, ', a.islower())
+print('é capitalizada ?, ', a.istitle())
