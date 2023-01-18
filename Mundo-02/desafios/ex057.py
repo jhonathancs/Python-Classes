8750bfadae634a1ae79be169d41a147b155bb42f
jhonathancs <jhonathan_jcs@hotmail.com>
2023-01-18 10:46:12 -0300

    aulas-14

---
+++ Mundo-02/desafios/ex057.py
@@ -0,0 +1,6 @@
+n = str(input('Informe seu sexo: [M/F]: ')).upper()
+while n != 'M':
+    n = str(input('Informe seu sexo: [M/F]: ')).upper()
+    # if n != 'M':
+    #     print(n)
+print('acabou')
