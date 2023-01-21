411ddc4bbac1d0b3158c1b54c25419a8bbe87bfc
jhonathancs <jhonathan_jcs@hotmail.com>
2023-01-21 16:19:12 -0300

    ex-057

--- Mundo-02/desafios/ex057.py
+++ Mundo-02/desafios/ex057.py
@@ -1,6 +1,6 @@
 n = str(input('Informe seu sexo: [M/F]: ')).upper()
-while n != 'M':
-    n = str(input('Informe seu sexo: [M/F]: ')).upper()
+while n not in 'MmFf':
+    n = str(input('Informe seu sexo: [M/F]: ')).strip().upper()[0]
     # if n != 'M':
     #     print(n)
-print('acabou')
+print('Sexo {} registrado com sucesso'.format(n))
